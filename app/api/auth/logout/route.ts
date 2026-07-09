db40027bb53ae9802fe0e21c67edf132a71ba5ab
tusharajput20 <rajputtushar6801@gmail.com>
2026-07-09 20:53:34 +0530

    feat(auth): complete authentication module with JWT and middleware

---
+++ app/api/auth/logout/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+    return NextResponse.json(
+        {
+            success: true,
+            message: "Logout successful.",
+        },
+        {
+            status: 200,
+        }
+    );
+}
